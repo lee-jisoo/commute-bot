af2ac8b715d6ba36fc568906e0458ab9155411eb
이지수 <leejisoo@webtoonscorp.com>
2022-08-17 18:46:08 +0900

    Initial commit

---
+++ src/main/java/com/example/commutebot/CommuteBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.commutebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommuteBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commutebot/CommuteBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commutebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuteBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
